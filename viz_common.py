f20d3283de7b08c6e088edb4328461e3a69b7df7
indiesoul2 <39783175+indiesoul2@users.noreply.github.com>
2019-05-13 09:25:29 +0900

    Add files via upload

---
+++ viz_common.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import seaborn as sns
+
+plt.rcParams["font.family"] = 'Malgun Gothic'
+plt.rcParams["figure.figsize"] = (14,4)
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams["axes.grid"] = True
+# 그래프에서 마이너스 폰트 깨지는 문제에 대한 대처
+plt.rcParams['axes.unicode_minus'] = False
+
